a0743bbf6be0ce703f7ae6f73d8a6b6e366a14d6
shaojiang <shaojiang.wang@mioying.com>
2020-07-05 20:47:01 +0800

    chore(app): update app

--- packages/ui-app/src/index.ts
+++ packages/ui-app/src/index.ts
@@ -1,7 +1,8 @@
 import { multi } from "jaminst-ui-lib";
+import { cyan } from "chalk";
 
 function main() {
   console.log(multi(1, 3));
-  console.log("app");
+  console.log(cyan("app"));
 }
 main();
