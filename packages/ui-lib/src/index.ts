65736212885824abc18722c49b84b6af32ce5cbd
shaojiang <shaojiang.wang@mioying.com>
2020-07-05 21:29:56 +0800

    feat(lib): update lib

--- packages/ui-lib/src/index.ts
+++ packages/ui-lib/src/index.ts
@@ -1,12 +1,16 @@
-import {add, multiply} from "lodash";
+import { add, multiply } from "lodash";
 
-export function multi(a:number,b:number){
-  console.log( add(a,b))
-  return multiply(a,b);
+export function multi(a: number, b: number) {
+  console.log(add(a, b));
+  return multiply(a, b);
 }
-export function divide(a:number, b:number){
-  return a/b;
+export function divide(a: number, b: number) {
+  return a / b;
 }
-export function minus(a:number,b:number){
-  return a-b;
+export function minus(a: number, b: number) {
+  return a - b;
+}
+
+export function foo() {
+  console.log("foo");
 }
